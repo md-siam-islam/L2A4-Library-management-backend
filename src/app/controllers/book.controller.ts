1841c04b2ff4ea18c492fe9d42c4e1a3a3a9dfd4
md-siam-islam <mdsiamislam663@gmail.com>
2025-07-01 23:01:19 -0700

    Book update  Oparetion Done

--- src/app/controllers/book.controller.ts
+++ src/app/controllers/book.controller.ts
@@ -1,6 +1,9 @@
 import express from "express"
 import { Request, Response } from "express";
 import Book from "../models/book.model";
+import { IBook } from "../interface/book.interface";
+
+import { Router } from "express";
 
 export const BookRoute = express.Router()
 
@@ -61,3 +64,41 @@ BookRoute.delete("/:id" , async(req: Request , res:Response)=> {
         data: deleteBook
     });
 })
+
+// Book update
+BookRoute.put('/:id', async (req:Request , res :Response) => {
+    
+    try {
+    const bookId = req.params.id ;
+    const {title, author, genre, isbn, description, copies} = req.body;
+
+        const book = await Book.findById(bookId);
+
+        if (!book) {
+            return res.status(404).json({ message: "Book not found" });
+        }
+
+        book.title = title || book.title;
+        book.author = author || book.author;
+        book.genre = genre || book.genre;
+        book.isbn = isbn || book.isbn;
+        book.description = description || book.description;
+        book.copies = copies || book.copies;
+        book.available = book.copies > 0;
+
+       const updatedBook = await book.save();
+
+        res.status(200).json({
+            success: true,
+            message: "Book updated successfully",
+            data: updatedBook
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error updating book",
+            error
+        });
+    }
+});
+
