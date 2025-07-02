74b064a134f8831d5b9d93b70bf1310ae16e44fe
md-siam-islam <mdsiamislam663@gmail.com>
2025-07-02 00:10:26 -0700

    Book Borrow and Borrow Book summary get oparetion done

--- src/app/controllers/borrowBook.controller.ts
+++ src/app/controllers/borrowBook.controller.ts
@@ -27,8 +27,7 @@ BorrowRouter.post('/' , async(req:Request , res:Response) => {
             success: false
         })
     }
-    const newBorrow = new Borrow({
-    ...Book.schema.obj, // Spread the book schema to include all fields
+    const newBorrow = new Borrow({// Spread the book schema to include all fields
       book: bookId,
       quantity,
       dueDate: new Date(dueDate)
@@ -55,3 +54,42 @@ BorrowRouter.post('/' , async(req:Request , res:Response) => {
         
     }
 })
+
+
+BorrowRouter.get('/summary' , async(req:Request , res: Response) => {
+    const borrowsummary = await Borrow.aggregate([
+        {
+            $group: {
+                _id: "$book",
+                totalBorrowed: { $sum: "$quantity" },
+                totalUsers: { $addToSet: "$user" }
+            }
+        },
+        {
+            $lookup: {
+                from: "books",
+                localField: "_id",
+                foreignField: "_id",
+                as: "bookDetails"
+            }
+        },
+        {
+            $unwind: "$bookDetails"
+        },
+        {
+            $project: {
+                _id: 0,
+                title: "$bookDetails.title",
+                author: "$bookDetails.author",
+                totalBorrowed: 1,
+                totalUsers: { $size: "$totalUsers" }
+            }
+        }
+    ]);
+
+    res.status(200).json({
+        message: 'Borrow summary retrieved successfully',
+        success: true,
+        data: borrowsummary
+    });
+});
